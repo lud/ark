4aaaf993f29f478107ee8f2b4d6e827cecd2f581
lud <ludovic@demblans.com>
2020-08-02 19:40:20 +0200

    added tests for ok?

--- test/ok_test.exs
+++ test/ok_test.exs
@@ -4,6 +4,7 @@ defmodule Ark.OkTest do
 
   test "wrapping ok" do
     assert {:ok, 1} = ok(1)
+    assert :ok = ok(:ok)
     assert {:ok, 1} = ok({:ok, 1})
     assert {:error, :reason} = ok({:error, :reason})
     assert {:error, :reason, :a} = ok({:error, :reason, :a})
@@ -13,6 +14,7 @@ defmodule Ark.OkTest do
   test "unwrapping ok" do
     assert 1 = uok({:ok, 1})
     assert 1 = uok(1)
+    assert :ok = uok(:ok)
     assert {:error, :reason} = uok({:error, :reason})
     assert {:error, :reason, :a} = uok({:error, :reason, :a})
     assert {:error, :reason, :a, :b} = uok({:error, :reason, :a, :b})
@@ -20,9 +22,19 @@ defmodule Ark.OkTest do
 
   test "unwrapping raise" do
     assert 1 = uok!({:ok, 1})
+    assert :ok = uok!(:ok)
     assert_raise Ark.Ok.UnwrapError, "Could not unwrap value: 1", fn -> uok!(1) end
     assert_raise Ark.Ok.UnwrapError, fn -> uok!({:error, :reason}) end
     assert_raise Ark.Ok.UnwrapError, fn -> uok!({:error, :reason, :a}) end
     assert_raise Ark.Ok.UnwrapError, fn -> uok!({:error, :reason, :a, :b}) end
+  end
+
+  test "questionning ok" do
+    refute ok?(1)
+    assert ok?(:ok)
+    assert ok?({:ok, 1})
+    refute ok?({:error, :reason})
+    refute ok?({:error, :reason, :a})
+    refute ok?({:error, :reason, :a, :b})
   end
 end
